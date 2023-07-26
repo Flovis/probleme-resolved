3fb2db6ee6f85ade8926a036afc3650f9c8c22f1
flovis <floviskabandafkm@gmail.com>
2023-07-27 01:55:54 +0200

    fonction day changed by adddind if the = today

--- day.js
+++ day.js
@@ -1,39 +1,31 @@
 function getDateForDayOfWeek(dayOfWeek) {
-    const days = [
-        "dimanche",
-        "lundi",
-        "mardi",
-        "mercredi",
-        "jeudi",
-        "vendredi",
-        "samedi",
-    ];
-    const today = new Date();
-    const currentDayOfWeek = today.getDay();
-    console.log(currentDayOfWeek);
-    const targetDayOfWeek = days.indexOf(dayOfWeek.toLowerCase());
-    console.log(targetDayOfWeek)
+  const days = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
+  const today = new Date();
+  const currentDayOfWeek = today.getDay();
+  const targetDayOfWeek = days.indexOf(dayOfWeek.toLowerCase());
 
-    if (targetDayOfWeek === -1) {
-        return "Jour de la semaine invalide";
-    }
+  if (targetDayOfWeek === -1) {
+    return 'Jour de la semaine invalide';
+  }
 
-    let daysToAdd = targetDayOfWeek - currentDayOfWeek;
-    if (daysToAdd <= 0) {
-        daysToAdd += 7;
-    }
+  let daysToAdd = targetDayOfWeek - currentDayOfWeek;
+  if (daysToAdd <= 0) {
+    daysToAdd += 7;
+  }
 
-    const targetDate = new Date(today);
-    targetDate.setDate(today.getDate() + daysToAdd);
-
-    // Formater la date au format 'YYYY-MM-DD'
-    const formattedDate = targetDate.toISOString().split("T")[0];
+  if (daysToAdd === 0) {
+    // Si le jour de la semaine demandé est aujourd'hui, retourner la date d'aujourd'hui
+    const formattedDate = today.toISOString().split('T')[0];
     return formattedDate;
+  }
+
+  const targetDate = new Date(today);
+  targetDate.setDate(today.getDate() + daysToAdd);
+  
+  // Formater la date au format 'YYYY-MM-DD'
+  const formattedDate = targetDate.toISOString().split('T')[0];
+  return formattedDate;
 }
 
-// Exemples d'utilisation :
-console.log(getDateForDayOfWeek("vendredi"));
-console.log(getDateForDayOfWeek("lundi"));
-console.log(getDateForDayOfWeek("samedi"));
-console.log(getDateForDayOfWeek("jeudi"));
-console.log(getDateForDayOfWeek("mercredi"));
+// Exemple d'utilisation pour un jeudi (aujourd'hui étant jeudi) :
+console.log(getDateForDayOfWeek('jeudi')); // Renvoie la date d'aujourd'hui
